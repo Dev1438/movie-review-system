c1ad4e15b6711525121a08092d97cdfc54b698c1
Dev1438 <devender.bakathatla@gmail.com>
2026-04-24 12:04:39 +0530

    remove clearable file input

--- movies/forms.py
+++ movies/forms.py
@@ -6,26 +6,26 @@ class MovieForm(forms.ModelForm):
         model = Movie
         fields = ['title', 'description', 'genre', 'rating', 'poster']
 
-    widgets = {
-        'title': forms.TextInput(attrs={
-            'class': 'form-control bg-black text-light border-secondary',
-            'placeholder': 'Enter movie title'
-        }),
-        'description': forms.Textarea(attrs={
-            'class': 'form-control bg-black text-light border-secondary',
-            'rows': 4
-        }),
-        'genre': forms.Select(attrs={
-            'class': 'form-select bg-black text-light border-secondary'
-        }),
-        'rating': forms.NumberInput(attrs={
-            'class': 'form-control bg-black text-light border-secondary',
-            'min': 1, 'max': 10, 'step': 0.1
-        }),
-        'poster': forms.ClearableFileInput(attrs={
-            'class': 'form-control bg-black text-light border-secondary'
-        }),
-    }
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control bg-black text-light border-secondary',
+                'placeholder': 'Enter movie title'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control bg-black text-light border-secondary',
+                'rows': 4
+            }),
+            'genre': forms.Select(attrs={
+                'class': 'form-select bg-black text-light border-secondary'
+            }),
+            'rating': forms.NumberInput(attrs={
+                'class': 'form-control bg-black text-light border-secondary',
+                'min': 1, 'max': 10, 'step': 0.1
+            }),
+            'poster': forms.FileInput(attrs={
+                'class': 'form-control bg-black text-light border-secondary'
+            }),
+        }
 
     def clean_rating(self):
         rating = self.cleaned_data.get('rating')
